5d0e463d8dd60505fdb82d9dcb56ce7557d16fcf
Grigorii Dzhanelidze <350804+strlght@users.noreply.github.com>
2021-05-17 13:30:35 +0300

    propagate finding both ways
    This soulution isn't really correct but will do for testing purposes right now.
    Instead it should work like this:
    	1. Find top level parent class defining the overriden method
    	2. Group all findings for this method, resolve duplicates (eg. by ignoring them)
    	3. Create overriding finding for each method in child classes

--- cutlass-core/src/main/kotlin/io/strlght/cutlass/core/DefaultFindingResolver.kt
+++ cutlass-core/src/main/kotlin/io/strlght/cutlass/core/DefaultFindingResolver.kt
@@ -50,13 +50,25 @@ class DefaultFindingResolver : FindingResolver {
     }
 
     private fun processOverridden(result: MutableList<Finding>, classMap: Map<String, ClassDef>) {
-        result.asSequence()
+        val childTypesMap = mutableMapOf<String, MutableSet<String>>()
+        classMap.values.forEach { classDef ->
+            val type = classDef.type
+            classDef.superclass?.also {
+                childTypesMap.getOrPut(it) { mutableSetOf() }.add(type)
+            }
+            classDef.interfaces.forEach {
+                childTypesMap.getOrPut(it) { mutableSetOf() }.add(type)
+            }
+        }
+        result
+            .toList() // copy the list to avoid CME
+            .asSequence()
             .filter { it is Finding.MethodName || it is Finding.FieldName }
             .forEach {
                 if (it is Finding.MethodName) {
-                    processOverriddenMethod(result, it, classMap)
+                    processOverriddenMethod(result, it, classMap, childTypesMap)
                 } else if (it is Finding.FieldName) {
-                    processOverriddenField(result, it, classMap)
+                    processOverriddenField(result, it, classMap, childTypesMap)
                 }
             }
     }
@@ -64,16 +76,24 @@ class DefaultFindingResolver : FindingResolver {
     private fun processOverriddenMethod(
         result: MutableList<Finding>,
         finding: Finding.MethodName,
-        classMap: Map<String, ClassDef>
+        classMap: Map<String, ClassDef>,
+        childTypesMap: Map<String, Set<String>>
     ) {
         processOverriddenMember(
             classMap = classMap,
-            root = finding.method.parent.value,
+            childTypesMap = childTypesMap,
+            startingClass = finding.method.parent.value,
             firstMatchingMember = { classDef ->
-                classDef.methods.firstOrNull { it.toCutlassModel() == finding.method }
+                classDef.methods.firstOrNull {
+                    val model = it.toCutlassModel()
+                    model.name == finding.method.name &&
+                        model.parameterTypes == finding.method.parameterTypes &&
+                        model.returnType == finding.method.returnType
+                }
             },
             onFound = {
-                result.add(finding.copy(method = it.toCutlassModel()))
+                result.add(finding.copy(method = it.toCutlassModel())
+                    .apply { source = FINDING_SOURCE })
             },
         )
     }
@@ -81,36 +101,77 @@ class DefaultFindingResolver : FindingResolver {
     private fun processOverriddenField(
         result: MutableList<Finding>,
         finding: Finding.FieldName,
-        classMap: Map<String, ClassDef>
+        classMap: Map<String, ClassDef>,
+        childTypesMap: MutableMap<String, MutableSet<String>>
     ) {
         processOverriddenMember(
             classMap = classMap,
-            root = finding.field.parent.value,
+            childTypesMap = childTypesMap,
+            startingClass = finding.field.parent.value,
             firstMatchingMember = { classDef ->
-                classDef.fields.firstOrNull { it.toCutlassModel() == finding.field }
+                classDef.fields.firstOrNull {
+                    val model = it.toCutlassModel()
+                    model.name == finding.field.name &&
+                        model.type == finding.field.type
+                }
             },
             onFound = {
-                result.add(finding.copy(field = it.toCutlassModel()))
+                result.add(finding.copy(field = it.toCutlassModel())
+                    .apply { source = FINDING_SOURCE })
             },
         )
     }
 
     private inline fun <reified T> processOverriddenMember(
         classMap: Map<String, ClassDef>,
-        root: String,
+        childTypesMap: Map<String, Set<String>>,
+        startingClass: String,
         crossinline firstMatchingMember: (ClassDef) -> T?,
         crossinline onFound: (T) -> Unit,
     ) {
-        val queue = mutableListOf(root)
+        // propagate to parent classes
+        visitMembers(
+            startingClass,
+            classMap,
+            firstMatchingMember,
+            onFound,
+            addToQueue = { queue, classDef ->
+                classDef.superclass?.also { queue.add(it) }
+                queue.addAll(classDef.interfaces)
+            },
+        )
+        // propagate to child classes
+        visitMembers(
+            startingClass,
+            classMap,
+            firstMatchingMember,
+            onFound,
+            addToQueue = { queue, classDef ->
+                childTypesMap[classDef.type]?.also { queue.addAll(it) }
+            },
+        )
+    }
+
+    private inline fun <reified T> visitMembers(
+        startingClass: String,
+        classMap: Map<String, ClassDef>,
+        crossinline firstMatchingMember: (ClassDef) -> T?,
+        crossinline onFound: (T) -> Unit,
+        crossinline addToQueue: (MutableList<String>, ClassDef) -> Unit,
+    ) {
+        val queue = mutableListOf(startingClass)
         while (queue.isNotEmpty()) {
             val type = queue.removeFirst()
             val classDef = classMap[type] ?: continue
+            addToQueue(queue, classDef)
             val member = firstMatchingMember(classDef) ?: continue
-            if (type != root) {
+            if (type != startingClass) {
                 onFound(member)
             }
-            classDef.superclass?.also { queue.add(it) }
-            queue.addAll(classDef.interfaces)
         }
+    }
+
+    companion object {
+        private const val FINDING_SOURCE = "resolved"
     }
 }
